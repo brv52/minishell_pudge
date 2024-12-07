a601cac0d587f6ebecd2249c6e22569de1492b70
ulitka223 <moonriver7609@gmail.com>
2024-12-07 05:30:02 +0100

    added EOF (Ctrl+D) support

--- input_handler.c
+++ input_handler.c
@@ -12,7 +12,21 @@ void	read_input(t_string *u_promt)
 
 	prep_promt_data(u_promt);
 	input = readline(__BEG_PROMT);
-	if (input)
+	
+    if (!input)
+    {
+        init_str(u_promt);
+        return ;
+    }
+    if (*input == '\0')
+    {
+        u_promt->data = malloc(1);
+        u_promt->data[0] = '\0';
+        u_promt->size = 0;
+        free(input);
+        return ;
+    }
+    if (input)
 	{
 		add_history(input);
 		*u_promt = create_string(input, ft_strlen(input));
